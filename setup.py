7e9e7f73a9f2e3f8c516ed4d2b19aeb1e0a2032a
cicwak <vapvapapvpva@yandex.ru>
2023-10-17 10:49:49 +0300

    fix: typo

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ setup(
     description='Easy logs mailing',  # Give a short description about your library
     author='Konstantin Ponomarev(cicwak)',  # Type in your name
     author_email='cicwak@gmail.com',  # Type in your E-Mail
-    url='https://github.com/cicwak/mailing-logs',  # Provide either the link to your github or to your website
+    url='https://github.com/cicwak/mailing-logs',  # Provide either the link to your GitHub or to your website
     download_url='https://github.com/cicwak/mailing-logs/archive/refs/tags/0.2.tar.gz',  # I explain this later on
     keywords=['MAILING', 'LOGS', 'BEST'],  # Keywords that define your package best
     install_requires=[  # I get to this in a second
         'requests',
     ],
     classifiers=[
-        'Programming Language :: Python :: 3',  # Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3',  # Specify which python versions that you want to support
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
